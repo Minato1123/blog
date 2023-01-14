c48a5e9fccc8a04329267ec95be662ad341e937f
Minato1123 <slamdunk0522@gmail.com>
2023-01-14 21:20:50 +0800

    fix: favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   base: '/blog/',
   description: 'A blog about my learning journey',
   head: [
-    ['link', { rel: "shortcut icon", href: "/.vitepress/images/favicon.ico"}],
+    ['link', { rel: "shortcut icon", href: "/favicon.ico"}],
   ],
   themeConfig: {
     sidebar:[
