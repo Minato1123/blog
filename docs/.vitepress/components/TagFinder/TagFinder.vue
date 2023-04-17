29b87bc23a6e4ed1e495e8c0c7c09d9f944c5187
Minato1123 <slamdunk0522@gmail.com>
2023-04-17 11:24:44 +0800

    feat: add clear tags btn title

--- docs/.vitepress/components/TagFinder/TagFinder.vue
+++ docs/.vitepress/components/TagFinder/TagFinder.vue
@@ -47,7 +47,7 @@ const currentData = computed(() => {
       <div class="input">
         <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="currentColor" d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5A6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14z"></path></svg>
         <input v-model="searchInput" placeholder="搜尋 Tag ..." type="text">
-        <button @click="handleClearSelectedTags">
+        <button @click="handleClearSelectedTags" title="清除所選 Tags">
           <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41z"></path></svg>
         </button>
       </div>
